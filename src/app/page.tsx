529844fad7cd84d5a057940de6984018b8faa581
Kurt Maglalang <kbm126@live.com>
2023-10-27 16:54:45 -0700

    feat: update the page to fill the rest of the screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className="">asdf</main>;
+  return (
+    <main className="flex flex-row flex-1 bg-slate-400 p-5 w-full h-full">
+      asdf
+    </main>
+  );
 }
