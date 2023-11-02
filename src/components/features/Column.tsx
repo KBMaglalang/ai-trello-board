64d1736dad2e674ab425e26697ba3f1058d51971
Kurt Maglalang <kbm126@live.com>
2023-11-02 15:51:29 -0700

    feat: column component

---
+++ src/components/features/Column.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+
+// components
+import Card from "./Card";
+import { useBoardStore } from "@/store/BoardStore";
+import { useModalStore } from "@/store/ModalStore";
+
+type Props = {
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
+};
+
+export default function Column({ id, todos, index }: Props) {
+  const [setNewTaskType, searchString] = useBoardStore((state) => [
+    state.setNewTaskType,
+    state.searchString,
+  ]);
+  const openModal = useModalStore((state) => state.openModal);
+
+  const handleAddTodo = () => {
+    setNewTaskType(id);
+    openModal();
+  };
+
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`pb-2
+                ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"}
+                  p-2 rounded-2xl shadow-sm
+                `}
+              >
+                <h2 className="flex justify-between p-2 text-xl font-bold">
+                  {idToColumnText[id]}
+                  <span className="px-2 py-1 text-sm font-normal text-gray-500 bg-gray-200 rounded-full">
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLowerCase()
+                            .includes(searchString.toLowerCase())
+                        ).length}
+                  </span>
+                </h2>
+                <div className="space-y-2">
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLocaleLowerCase())
+                    ) {
+                      return null; // Skip rendering this todo
+                    }
+
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <Card
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                  <div className="flex items-end justify-end p-2">
+                    <button
+                      onClick={handleAddTodo}
+                      className="text-green-500 hover:text-green-600"
+                    >
+                      <PlusCircleIcon className="w-10 h-10" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  );
+}
