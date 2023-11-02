45ce7967e3d44b28b8f79c79f4fcbea0fdaa35e5
Kurt Maglalang <kbm126@live.com>
2023-11-02 15:50:07 -0700

    feat: setup board on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+// component
+import Board from "@/components/features/Board";
+
 export default function Home() {
   return (
     <main className="flex flex-row flex-1 bg-slate-400 p-5 w-full h-full">
-      asdf
+      <Board />
     </main>
   );
 }
