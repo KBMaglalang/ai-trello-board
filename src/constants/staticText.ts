ee82a824b467e9ec64f1d6f31795e069fda7c4df
Kurt Maglalang <kbm126@live.com>
2023-11-03 19:22:39 -0700

    feat: update name to TaskAi

--- src/constants/staticText.ts
+++ src/constants/staticText.ts
@@ -1,3 +1,3 @@
-export const PROJECT_NAME = "AI Trello Board";
+export const PROJECT_NAME = "TaskAI";
 export const PROJECT_DESCRIPTION =
-  "Use the power of AI to manage your Trello board";
+  "Use the power of AI to manage your board and tasks";
