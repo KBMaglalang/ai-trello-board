c44d002e1f53d664a4fbe3b2e359a5c2c6ad9b61
Kurt Maglalang <kbm126@live.com>
2023-11-03 18:29:43 -0700

    feat: set priority group to select what was chosen in the card

--- src/components/features/TaskPriorityGroup.tsx
+++ src/components/features/TaskPriorityGroup.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useBoardStore } from "@/store/BoardStore";
+import { useModalStore } from "@/store/ModalStore";
 
 export default function TaskPriorityGroup() {
+  const [isEditModal, cardInfo] = useModalStore((state) => [
+    // states
+    state.isEditModal,
+    state.cardInfo,
+  ]);
   const [newTaskPriority, setNewTaskPriority] = useBoardStore((state) => [
     state.newTaskPriority,
     state.setNewTaskPriority,
   ]);
+
+  useEffect(() => {
+    if (isEditModal) {
+      setNewTaskPriority(cardInfo?.todo?.priority);
+    }
+  }, [isEditModal, cardInfo?.todo?.priority, setNewTaskPriority]);
 
   const handleSelect = (e: any) => {
     const value = e.target.value.toLowerCase();
@@ -25,10 +37,13 @@ export default function TaskPriorityGroup() {
     <select
       className="select select-bordered w-full border-gray-300 rounded-md mt-2"
       onChange={handleSelect}
+      value={
+        !newTaskPriority
+          ? "Select a Task Priority"
+          : newTaskPriority[0].toUpperCase() + newTaskPriority.slice(1)
+      }
     >
-      <option disabled selected>
-        Select a Task Priority
-      </option>
+      <option disabled>Select a Task Priority</option>
       <option>Low</option>
       <option>Medium</option>
       <option>High</option>
