cd62c874ee067fd6f185f9d99aa16ca7de3ea998
Kurt Maglalang <kbm126@live.com>
2023-11-07 11:11:56 -0800

    feat: pass board id to the board component

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -3,10 +3,16 @@ import React from "react";
 // components
 import Board from "@/components/features/Board";
 
-export default function page() {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function page({ params: { id } }: Props) {
   return (
     <main className="flex flex-row flex-1 p-5 w-full h-full">
-      <Board />
+      <Board id={id} />
     </main>
   );
 }
