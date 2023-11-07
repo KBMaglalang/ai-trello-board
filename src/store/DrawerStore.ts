5d682af9a1c3e07d9c97408fa1f346e510e86b50
Kurt Maglalang <kbm126@live.com>
2023-11-06 20:43:03 -0800

    feat: create drawer state store

---
+++ src/store/DrawerStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface DrawerState {
+  isOpen: boolean;
+
+  toggleDrawer: () => void;
+  openDrawer: () => void;
+  closeDrawer: () => void;
+}
+
+export const useDrawerStore = create<DrawerState>((set, get) => ({
+  isOpen: false,
+
+  toggleDrawer: () => set((state) => ({ isOpen: !state.isOpen })),
+  openDrawer: () => set({ isOpen: true }),
+  closeDrawer: () => set({ isOpen: false }),
+}));
