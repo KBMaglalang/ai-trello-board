5bedcb1735a46b4042a2cf2cac3588a4bd5b48a7
Kurt Maglalang <kbm126@live.com>
2023-11-08 11:27:13 -0800

    feat: update  code to use newboardStore

--- src/lib/appwrite/cards/updateCard.ts
+++ src/lib/appwrite/cards/updateCard.ts
@@ -3,10 +3,6 @@
 import { databases } from "@/config/appwrite";
 
 export const updateCard = async (id: string, updateData: any) => {
-  console.log(
-    "🚀 ~ file: updateCard.ts:6 ~ updateCard ~ cardData:",
-    updateData
-  );
   const data = await databases.updateDocument(
     process.env.NEXT_PUBLIC_DATABASE_ID!,
     process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
@@ -15,7 +11,6 @@ export const updateCard = async (id: string, updateData: any) => {
       ...updateData,
     }
   );
-  console.log("🚀 ~ file: updateCard.ts:15 ~ updateCard ~ data:", data);
 
   return data;
 };
