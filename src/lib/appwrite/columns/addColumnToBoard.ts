6e27f69f8accd38e0d8b265518db046b09aadc3b
Kurt Maglalang <kbm126@live.com>
2023-11-08 11:26:03 -0800

    fix: set columnArray to any

--- src/lib/appwrite/columns/addColumnToBoard.ts
+++ src/lib/appwrite/columns/addColumnToBoard.ts
@@ -1,9 +1,6 @@
 import { databases, ID } from "@/config/appwrite";
 
-export const addColumnToBoard = async (
-  boardId: string,
-  columnArray: string[]
-) => {
+export const addColumnToBoard = async (boardId: string, columnArray: any[]) => {
   const data = await databases.updateDocument(
     process.env.NEXT_PUBLIC_DATABASE_ID!,
     process.env.NEXT_PUBLIC_BOARDS_COLLECTION_ID!,
