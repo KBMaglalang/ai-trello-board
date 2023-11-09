edc067f97d0928949fecfde38b1a570167bad7d6
Kurt Maglalang <kbm126@live.com>
2023-11-09 14:14:35 -0800

    feat: update modal to use daisy UI

--- src/components/features/Modal.tsx
+++ src/components/features/Modal.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { FormEvent, Fragment, useRef, useEffect, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 // components
-import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
 import TaskPriorityGroup from "./TaskPriorityGroup";
 import TaskDatePicker from "./TaskDatePicker";
 
@@ -18,6 +16,7 @@ import { useNewBoardStore } from "@/store/NewBoardStore";
 // constants and functions
 import getUrl from "@/lib/getUrl";
 import { createCard, updateCard, addCardToColumn } from "@/lib/appwrite/cards";
+import { closeTaskModal } from "@/lib/util";
 
 function Modal() {
   const imagePickerRef = useRef<HTMLInputElement>(null);
@@ -111,9 +110,11 @@ function Modal() {
 
   const handleOnClose = async () => {
     closeModal();
+    closeTaskModal();
+
+    setImage(null);
 
     // ! old
-    setImage(null);
     clearNewTaskStates();
     clearCardInfo();
 
@@ -122,8 +123,9 @@ function Modal() {
     await getBoardList();
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+
     if (!newTaskInput) return;
 
     if (isEditModal) {
@@ -165,92 +167,47 @@ function Modal() {
         ...workingColumn.todos,
         newCardData,
       ]);
-
-      // addTask(
-      //   newTaskInput,
-      //   newTaskDescription,
-      //   newTaskPriority,
-      //   newTaskType,
-      //   image,
-      //   newTaskStartDate,
-      //   newTaskEndDate
-      // );
     }
 
     handleOnClose();
   };
 
   return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog
-        as="form"
-        onSubmit={handleSubmit}
-        className="relative z-10"
-        onClose={handleOnClose}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
+    <dialog id="task_modal" className="modal">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg text-center">
+          {isEditModal ? "Edit Task" : "Add a Task"}
+        </h3>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-full p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <Dialog.Title
-                  as="h3"
-                  className="pb-2 text-lg font-medium leading-6 text-gray-900 text-center"
-                >
-                  {isEditModal ? "Edit Task" : "Add a Task"}
-                </Dialog.Title>
+        {/* task title input */}
+        <div className="mt-2">
+          <input
+            type="text"
+            value={newTaskInput}
+            onChange={(e) => setNewTaskInput(e.target.value)}
+            placeholder="Enter a task here..."
+            className="input w-full p-5 border border-gray-300 rounded-md outline-none"
+          />
+        </div>
 
-                {/* task title input */}
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    value={newTaskInput}
-                    onChange={(e) => setNewTaskInput(e.target.value)}
-                    placeholder="Enter a task here..."
-                    className="w-full p-5 border border-gray-300 rounded-md outline-none"
-                  />
-                </div>
+        {/* task description input */}
+        <div className="mt-2">
+          <textarea
+            value={newTaskDescription}
+            onChange={(e) => setNewTaskDescription(e.target.value)}
+            placeholder="Enter a description here..."
+            className="textarea textarea-bordered w-full p-5 border border-gray-300 rounded-md outline-none"
+          />
+        </div>
 
-                {/* task description input */}
-                <div className="mt-2">
-                  <textarea
-                    value={newTaskDescription}
-                    onChange={(e) => setNewTaskDescription(e.target.value)}
-                    placeholder="Enter a description here..."
-                    className="w-full p-5 border border-gray-300 rounded-md outline-none"
-                  />
-                </div>
+        {/* start and end date */}
+        <TaskDatePicker />
 
-                {/* start and end date */}
-                <TaskDatePicker />
+        {/* select priority: null, low, medium, high */}
+        <TaskPriorityGroup />
 
-                {/* radio group: null, low, medium, high */}
-                <TaskPriorityGroup />
-
-                {/* column type: todo, in progress, or done */}
-                {/* <TaskTypeRadioGroup /> */}
-
-                {/* File Input goes here... */}
-                {/* <div className="mt-2">
+        {/* File Input goes here... */}
+        {/* <div className="mt-2">
                   {image ? (
                     <Image
                       priority
@@ -291,21 +248,20 @@ function Modal() {
                   )}
                 </div> */}
 
-                <div className="mt-4">
-                  <button
-                    type="submit"
-                    disabled={!newTaskInput}
-                    className="w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-not-allowed"
-                  >
-                    {isEditModal ? "Update Task" : "Add Task"}
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
+        <button
+          onClick={handleSubmit}
+          disabled={!newTaskInput}
+          className="btn btn-primary w-full inline-flex mt-4"
+        >
+          {isEditModal ? "Update Task" : "Add Task"}
+        </button>
+      </div>
+
+      {/* clicking outside of the modal */}
+      <form method="dialog" className="modal-backdrop">
+        <button></button>
+      </form>
+    </dialog>
   );
 }
 
