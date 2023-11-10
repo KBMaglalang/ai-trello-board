52776dea37d754cf131f6f51df7b131c68d51f50
Kurt Maglalang <kbm126@live.com>
2023-11-09 17:12:26 -0800

    feat: sort homepage boards by the update At time

--- src/components/features/HomepageBoardList/BoardList.tsx
+++ src/components/features/HomepageBoardList/BoardList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // components
 import { BoardListItem } from ".";
@@ -14,13 +14,29 @@ import { useNewBoardStore } from "@/store/NewBoardStore";
 export function BoardList() {
   const [boardList] = useNewBoardStore((state) => [state.boardList]);
 
+  const [sortedBoardList, setSortedBoardList] = useState([]);
+
+  useEffect(() => {
+    if (boardList) {
+      // sort the boardlist by the $updatedAt property
+      const sortedList = boardList.sort((a, b) => {
+        return (
+          new Date(b?.$updatedAt).getTime() - new Date(a?.$updatedAt).getTime()
+        );
+      });
+
+      setSortedBoardList(sortedList);
+    }
+  }, [boardList]);
+
   return (
     <div className="flex flex-wrap gap-4 items-center justify-center">
-      {boardList &&
-        boardList.map((board) => (
-          <BoardListItem key={board.$id} boardData={board} />
+      {sortedBoardList &&
+        sortedBoardList.map((board) => (
+          <BoardListItem key={board.$id!} boardData={board} />
         ))}
 
+      {/* create new board button */}
       <CreateNewBoard />
     </div>
   );
