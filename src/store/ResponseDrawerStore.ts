0a3632812616de9dda36db807736830c208215d4
Kurt Maglalang <kbm126@live.com>
2023-11-22 14:11:49 -0800

    feat: create board store

---
+++ src/store/ResponseDrawerStore.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+
+interface DrawerState {
+  responseDrawerOpen: boolean;
+  toggleResponseDrawer: () => void;
+  openResponseDrawer: () => void;
+  closeResponseDrawer: () => void;
+
+  responseSummary: string;
+  setResponseSummary: (summary: string) => void;
+  clearResponseSummary: () => void;
+
+  responseBreakdown: string;
+  setResponseBreakdown: (breakdown: string) => void;
+  clearResponseBreakdown: () => void;
+
+  responseLoading: boolean;
+  setResponseLoading: (loading: boolean) => void;
+  clearResponseLoading: () => void;
+}
+
+export const useResponseDrawerStore = create<DrawerState>((set, get) => ({
+  // --- Response Drawer ---
+  responseDrawerOpen: false,
+  toggleResponseDrawer: () =>
+    set((state) => ({ responseDrawerOpen: !state.responseDrawerOpen })),
+  openResponseDrawer: () => set({ responseDrawerOpen: true }),
+  closeResponseDrawer: () => set({ responseDrawerOpen: false }),
+
+  // --- Response Summary ---
+  responseSummary: "",
+  setResponseSummary: (summary: string) => set({ responseSummary: summary }),
+  clearResponseSummary: () => set({ responseSummary: "" }),
+
+  // --- Response Breakdown ---
+  responseBreakdown: "",
+  setResponseBreakdown: (breakdown: string) =>
+    set({ responseBreakdown: breakdown }),
+  clearResponseBreakdown: () => set({ responseBreakdown: "" }),
+
+  // --- Response Loading ---
+  responseLoading: false,
+  setResponseLoading: (loading: boolean) => set({ responseLoading: loading }),
+  clearResponseLoading: () => set({ responseLoading: false }),
+}));
